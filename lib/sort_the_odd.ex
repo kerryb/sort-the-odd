32a13df7cbea85548aacb527c1c45660386cce14
Kerry Buckley <kerryjbuckley@gmail.com>
2020-10-16 18:04:49 +0100

    New project

---
+++ lib/sort_the_odd.ex
@@ -0,0 +1,18 @@
+defmodule SortTheOdd do
+  @moduledoc """
+  Documentation for `SortTheOdd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SortTheOdd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sort_the_odd_test.exs
@@ -0,0 +1,8 @@
+defmodule SortTheOddTest do
+  use ExUnit.Case
+  doctest SortTheOdd
+
+  test "greets the world" do
+    assert SortTheOdd.hello() == :world
+  end
+end
