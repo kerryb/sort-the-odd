9a67fb2da9e68955f031233374157aadd2d687f1
Kerry Buckley <kerryjbuckley@gmail.com>
2020-10-16 18:10:03 +0100

    Remove generated boilerplate code

--- lib/sort_the_odd.ex
+++ lib/sort_the_odd.ex
@@ -1,18 +1,2 @@
 defmodule SortTheOdd do
-  @moduledoc """
-  Documentation for `SortTheOdd`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SortTheOdd.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/sort_the_odd_test.exs
+++ test/sort_the_odd_test.exs
@@ -1,8 +1,3 @@
 defmodule SortTheOddTest do
   use ExUnit.Case
-  doctest SortTheOdd
-
-  test "greets the world" do
-    assert SortTheOdd.hello() == :world
-  end
 end
